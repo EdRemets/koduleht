2566f8b339d9f67a4f7282e89a768a6bcfaa3740
Eduard Remets <Eduard.Remets@tpt.lan>
2026-02-06 15:13:44 +0200

    add mult

--- script.js
+++ script.js
@@ -4,9 +4,12 @@ const countText = document.querySelector(".click-count");
 const randomParagraph = document.querySelector(".randomParagraph");
 const autoClickerBtn = document.querySelector(".autoClickerBtn");
 const autoClickerImg = document.querySelector(".autoClickerImg");
-let count = localStorage.getItem('clickCount') ? localStorage.getItem('clickCount') : 0;
-let autoclickCount = localStorage.getItem('autoclickCount') ? localStorage.getItem('autoclickCount') : 0;;
+const clickMultBtn = document.querySelector(".clickMultBtn");
+let count = 0;
+count = localStorage.getItem('clickCount') ? localStorage.getItem('clickCount') : 0;
 let clickInterval = null;
+let ownedItems = localStorage.getItem('ownedItems') ? JSON.parse(localStorage.getItem('ownedItems')) : {};
+let clickingTime = 1 / ownedItems['Autoclicker'] <= 0 ? 1 : ownedItems['Autoclicker'];
 
 function randomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
@@ -27,18 +30,38 @@ function getText() {
     return "no text";
 };
 
+function getAutoclickerCount() {
+    return ownedItems['Autoclicker'] ? ownedItems['Autoclicker'] : 0;
+}
+
+function getAutoclickerCost() {
+    return getAutoclickerCount() <= 0 ? 10 : Math.round(10 * getAutoclickerCount() * 1.15);
+}
+
+function getMultiplierCount() {
+    return ownedItems['Multiplier'] ? ownedItems['Multiplier'] : 0;
+}
+
+function getMultiplierCost() {
+    return getMultiplierCount() <= 0 ? 30 : Math.round(30 * getMultiplierCount() * 1.15);
+}
+
 function clickButton() {
-    ++count;
+    const multCount = getMultiplierCount();
+    if (multCount > 0) {
+        count = Number(count) + 1 * getMultiplierCount();
+    }
+    else {
+        count++;
+    }
     update();
 }
 
-let clickingTime = 1;
 
-console.log(clickingTime)
 
 function resetInterval() {
     clearInterval(clickInterval);
-    clickingTime = 1 / autoclickCount;
+    clickingTime = 1 / ownedItems['Autoclicker'];
     clickInterval = setInterval(function() {
         clickButton();
     }, clickingTime * 1000);
@@ -47,7 +70,7 @@ function resetInterval() {
 function update() {
     countText.textContent = "Count: " + count;
     localStorage.setItem('clickCount', count);
-    if (autoclickCount != 0 && clickInterval == null) {
+    if (ownedItems['Autoclicker'] > 0) {
         resetInterval();
     }
 }
@@ -61,13 +84,30 @@ clickBtn.addEventListener('click', function() {
 });
 
 autoClickerBtn.addEventListener('click', function() {
-    if (count > 9) {
-        count -= 10
-        autoclickCount++;
-        localStorage.setItem('autoclickCount', autoclickCount);
+    let price = getAutoclickerCost();
+    if (count >= price) {
+        count -= price;
+        ownedItems['Autoclicker'] == null ? ownedItems['Autoclicker'] = 1 : ownedItems['Autoclicker']++;
+        localStorage.setItem('ownedItems', JSON.stringify(ownedItems));
         update();
+        autoClickerBtn.textContent = 'Autoclicker\r\nCosts: ' + getAutoclickerCost();
+    }
+    
+});
+
+clickMultBtn.addEventListener('click', function() {
+    let price = getMultiplierCost();
+    if (count >= price) {
+        count -= price;
+        ownedItems['Multiplier'] == null ? ownedItems['Multiplier'] = 2 : ownedItems['Multiplier']++;
+        localStorage.setItem('ownedItems', JSON.stringify(ownedItems));
+        update();
+        clickMultBtn.textContent = '2x mult\r\nCosts: ' + getMultiplierCost();
     }
     
 });
 
 update();
+console.log(autoClickerBtn.textContent);
+autoClickerBtn.textContent = 'Autoclicker\r\nCosts: ' + getAutoclickerCost();
+clickMultBtn.textContent = '2x mult\r\nCosts: ' + getMultiplierCost();
